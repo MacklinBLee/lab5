e31d34ba1b99ebd1d409fa3120554192075f69db
Macklin Lee <macklinblee@gmail.com>
2016-02-04 00:17:26 -0800

    putting it together lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$("a.nameid").click(projectClick);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,12 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+}
+
+function projectClick(e) {
+   
+    e.preventDefault();   
+    var newName = $(this).text();
+	 newName= anagrammedName(newName);
+	$(this).text(newName);
 }
